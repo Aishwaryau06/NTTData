35b60a810a88de463e6750b8a71798e6053e3f43
Aishwaryau06 <193250@nttdata.com>
2020-03-26 12:01:06 +0530

    this is my first commit

---
+++ FactoryDesignP/FactoryDesignP/CardFactory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignP
+{
+    abstract class CardFactory
+    {
+        public abstract CreditCard GetCreditCard();
+    }
+
+    class MoneyBackFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public MoneyBackFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard()
+        {
+            return new MoneyBackCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+
+
+    class TitaniumFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public TitaniumFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard()
+        {
+            return new TitaniumCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+
+
+    class PlatinumFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public PlatinumFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard()
+        {
+            return new PlatinumCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+}
+
+
+    
