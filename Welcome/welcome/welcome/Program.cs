cc0140854919ad8a025f85a3e1b43359caf5bd82
Aishwaryau06 <193250@nttdata.com>
2020-03-26 15:09:08 +0530

    this is my second commit

---
+++ Welcome/welcome/welcome/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+            Console.WriteLine("Enter your Name");
+            username = Console.ReadLine();
+            Console.WriteLine("HELLO WELCOME {0}", username);
+            Console.ReadLine();
+        }
+    }
+}
